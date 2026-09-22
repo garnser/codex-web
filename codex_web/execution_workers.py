b0e1c5472250782fb74507ec4b7894583023a22c
Jonathan Petersson <jpetersson@garnser.se>
2026-09-22 08:44:52 +0200

    Bump execution-worker state for canonical failures

--- codex_web/execution_workers.py
+++ codex_web/execution_workers.py
@@ -19,8 +19,8 @@ from codex_web.resources import RepositoryExecutionTarget
 
 EXECUTION_WORKER_CONTRACT = ContractSpec(
     "execution-worker-state",
-    "1.5",
-    ("1.0", "1.1", "1.2", "1.3", "1.4", "1.5"),
+    "1.6",
+    ("1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6"),
 )
 
 
